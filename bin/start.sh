b13d61a3105265f855abc66261b8d2eabbe3350e
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2022-04-05 15:30:51 -0400

    update digitalocean

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=US/ST=NY/O=Company/CN=example.com" -keyout openbattles.key -out openbattles.crt && \
 gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4
