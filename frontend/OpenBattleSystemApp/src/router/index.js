c45bfca70bee41a21d803d6062ccaf80db5e5cf3
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2022-04-20 20:00:27 -0400

    Update index.js

--- frontend/OpenBattleSystemApp/src/router/index.js
+++ frontend/OpenBattleSystemApp/src/router/index.js
@@ -2,9 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import indexView from '../views/indexView.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  mode: "hash",
+    history: createWebHistory(import.meta.env.BASE_URL),
+  // mode: "hash",
   routes: [
+    {
+      path: "/:catchAll(.*)",
+      name: 'catchall',
+      component: indexView
+    },
     {
       path: '/',
       name: 'index',
