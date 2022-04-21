228c636c1d6228f58335ef66d07748206f1e1ec2
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2022-04-21 14:58:15 -0400

    Update Contents.vue

--- frontend/OpenBattleSystemApp/src/views/rules/core/Contents.vue
+++ frontend/OpenBattleSystemApp/src/views/rules/core/Contents.vue
@@ -14,11 +14,15 @@ import NavBar from '@/components/NavBar.vue';
         </template>
     </NavBar>
     <div class="container-md mt-5">
-        <router-link to="/rules/core/playingsurface" class="text-decoration-none text-dark fs-3"><div class="border-bottom border-top d-flex flex-row p-3"><div class="d-flex flex-fill">Playing Surface</div><div class="d-inline-flex"> > </div></div></router-link>
-        <router-link to="/rules/core/unitsandgear" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="d-flex flex-fill">Units and Gear</div><div class="d-inline-flex"> > </div></div></router-link>
-        <router-link to="/rules/core/squads" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="d-flex flex-fill">Squads</div><div class="d-inline-flex"> > </div></div></router-link>
-        <router-link to="/rules/core/gameround" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="]d-flex flex-fill">Game Round</div><div class="d-inline-flex"> > </div></div></router-link>
-        <router-link to="/rules/core/formulas" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="d-flex flex-fill">Formulas</div><div class="d-inline-flex"> > </div></div></router-link>
+        <div class="row">
+            <div class="col">
+                <router-link to="/rules/core/playingsurface" class="text-decoration-none text-dark fs-3"><div class="border-bottom border-top d-flex flex-row p-3"><div class="d-flex flex-fill">Playing Surface</div><div class="d-inline-flex"> > </div></div></router-link>
+                <router-link to="/rules/core/unitsandgear" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="d-flex flex-fill">Units and Gear</div><div class="d-inline-flex"> > </div></div></router-link>
+                <router-link to="/rules/core/squads" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="d-flex flex-fill">Squads</div><div class="d-inline-flex"> > </div></div></router-link>
+                <router-link to="/rules/core/gameround" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="]d-flex flex-fill">Game Round</div><div class="d-inline-flex"> > </div></div></router-link>
+                <router-link to="/rules/core/formulas" class="text-decoration-none text-dark fs-3"><div class="border-bottom d-flex flex-row p-3"><div class="d-flex flex-fill">Formulas</div><div class="d-inline-flex"> > </div></div></router-link>
+            </div>
+        </div>
     </div>
 </div>
 </template>
