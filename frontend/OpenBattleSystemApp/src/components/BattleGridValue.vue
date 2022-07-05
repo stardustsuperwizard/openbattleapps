70586580d074d577e6421d761b6340a25ce80a07
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2022-07-05 00:55:25 -0400

    Update BattleGridValue.vue

--- frontend/OpenBattleSystemApp/src/components/BattleGridValue.vue
+++ frontend/OpenBattleSystemApp/src/components/BattleGridValue.vue
@@ -1,25 +1,21 @@
 <script setup>
-import { computed, defineProps, ref } from "vue";
+import { computed } from "vue";
 
 const props = defineProps({
     skillAttributeImported: {
-        type: String,
+        type: Number,
         default: 0
     },
     difficultyImported: {
-        type: String,
+        type: Number,
         default: 0
     }
 });
 
-
-const skillAttribute = ref(props.skillAttributeImported.value);
-const difficulty = ref(props.difficultyImported.value);
-
 const diceRollRequired = computed(() => {
-    console.log(skillAttribute.value);
-    console.log(props.difficultyImported.value);
-    let score = parseInt(skillAttribute.value) - parseInt(difficulty.value);
+    // console.log(props.skillAttributeImported);
+    // console.log(difficulty.value);
+    let score = parseInt(props.skillAttributeImported) - parseInt(props.difficultyImported);
     if (score === -3) {
         return '6 (roll second die, 6 required)'
     } else if (score === -2) {
@@ -45,7 +41,6 @@ const diceRollRequired = computed(() => {
     <div class="form-floating">
         <input type="text" name="gearTotalCost" id="gearTotalCost" class="form-control" v-bind:value="diceRollRequired" disabled>
         <label for="gearTotalCost">Required Roll</label>
-        {{ skillAttributeImported }}
     </div>
 </template>
 
