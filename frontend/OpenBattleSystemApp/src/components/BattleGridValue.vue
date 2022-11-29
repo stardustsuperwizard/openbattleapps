79a50946e23c76a333885aa85ac059e7b3b7c759
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2022-11-29 07:59:27 -0500

    updated grid

--- frontend/OpenBattleSystemApp/src/components/BattleGridValue.vue
+++ frontend/OpenBattleSystemApp/src/components/BattleGridValue.vue
@@ -15,7 +15,25 @@ const props = defineProps({
 const diceRollRequired = computed(() => {
     // console.log(props.skillAttributeImported);
     // console.log(difficulty.value);
-    
+    let score = skillAttribute.value - difficulty.value;
+    if (score >= 9) {
+        return '2+ (Re-roll failed dice)'
+    } else {
+        let dividend = skillAttribute.value / difficulty.value;
+        if (dividend < 0.5) {
+            return 'Not possible.'
+        } else if (dividend === 0.5) {
+            return '6'
+        } else if (dividend > 0.5 && dividend < 1) {
+            return '5+'  
+        } else if (dividend === 1) {
+            return '4+'
+        } else if (dividend > 1 && dividend < 2) {
+            return '3+'
+        } else if (dividend >= 2) {
+            return '2+'
+        }  
+    }
     
 });
 </script>
